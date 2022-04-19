ff404e31c33ccfb01991b33acb62b747a7ef7373
Walker Denial <WalkerDenial@qq.com>
2022-04-19 17:24:10 +0800

    feat: config code edit

---
+++ src/utils/request.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const service = axios.create({
+
+});
+
+service.interceptors.request.use(
+    config => {
+        return config;
+    }
+);
+
+service.interceptors.response.use(
+    response => {
+
+    }
+);
+
+export default service;
