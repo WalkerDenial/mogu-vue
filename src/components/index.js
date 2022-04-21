f481a7f7980699b77b00d6be6f5864534bc87bc3
Walker Denial <WalkerDenial@qq.com>
2022-04-21 17:31:34 +0800

    feat: layout 开发

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,26 @@
+import { Layout, Header, Footer, Aside, Main } from './layout';
 import Col from 'vant/lib/col';
 import 'vant/lib/col/index.css';
 import Row from 'vant/lib/row';
 import 'vant/lib/row/index.css';
 
 const components = {
+    Layout,
+    Header,
+    Footer,
+    Aside,
+    Main,
     CRow: Row,
     CCol: Col,
 }
 
 const install = (app) => {
   Object.keys(components).forEach(key => {
-      app.component(key, components[key])
+    if (key === 'CRow' || key === 'CCol') {
+      app.component(key, components[key]);
+    } else {
+      app.component(components[key]['name'], components[key]);
+    }
   });
 }
 
---
+++ src/components/layout/index.js
@@ -0,0 +1,7 @@
+import Aside from './aside.vue';
+import Footer from './footer.vue';
+import Header from './header.vue';
+import Layout from './layout.vue';
+import Main from './main.vue';
+
+export { Aside, Footer, Header, Layout, Main }
