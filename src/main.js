c2b9fe776a1ad32d5a25e618834f76630ea11417
Walker Denial <WalkerDenial@qq.com>
2022-04-27 23:33:57 +0800

    feat: 字蛛处理

--- src/main.js
+++ src/main.js
@@ -8,5 +8,6 @@ import Mui from "@/components";
 import "@/mock";
 import "lib-flexible/flexible.js";
 import "@icon-park/vue/styles/index.css"
+import "../static/index.css"
 
 createApp(App).use(store).use(router).use(Mui).mount("#app");
